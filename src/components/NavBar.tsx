b0c16148b9e24993d0adee358428d108dbb24434
akshat <akshatsinha97@gmail.com>
2023-01-05 05:26:24 +0530

    title logo changed to inshorts

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -117,7 +117,7 @@ function NavBar() {
               textDecoration: "none",
             }}
           >
-            LOGO
+            InShorts
           </Typography>
           <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
             {pages.map((page) => (
