11796948b73034567061708d5698716967640e09
akshat <akshatsinha97@gmail.com>
2023-01-05 04:50:03 +0530

    navbar position sticky

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -42,7 +42,7 @@ function NavBar() {
   };
 
   return (
-    <AppBar position="static">
+    <AppBar position="sticky">
       <Container maxWidth="xl">
         <Toolbar disableGutters>
           <AdbIcon sx={{ display: { xs: "none", md: "flex" }, mr: 1 }} />
